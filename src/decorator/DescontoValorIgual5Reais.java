bbc898e93f7b222e7f9893552e30d8f46c97c9ad
gustavo <gustavodreche@msn.com>
2020-04-04 17:48:55 -0300

    Padrao Decorator

---
+++ src/decorator/DescontoValorIgual5Reais.java
@@ -0,0 +1,17 @@
+package decorator;
+
+public class DescontoValorIgual5Reais extends Desconto {
+
+	public DescontoValorIgual5Reais() {
+		super();
+	}
+	
+	public DescontoValorIgual5Reais(Desconto outroDesconto) {
+		super(outroDesconto);
+	}
+	
+	public double desconto(double valor) {
+		return valor * 0.2 + calculoDoOutroDesconto(valor);
+	}
+
+}
