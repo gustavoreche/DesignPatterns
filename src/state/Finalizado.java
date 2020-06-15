069f747d44844e18ac164f330bd03210728c7a58
gustavo <gustavodreche@msn.com>
2020-06-15 12:56:17 -0300

    Padrao State

---
+++ src/state/Finalizado.java
@@ -0,0 +1,25 @@
+package state;
+
+public class Finalizado implements EstadoDeUmOrcamento {
+
+	@Override
+	public void aplicaDescontoExtra(Orcamento orcamento) {
+		throw new RuntimeException("Orçamento está finalizado");
+	}
+
+	@Override
+	public void aprova(Orcamento orcamento) {
+		throw new RuntimeException("Orçamento está finalizado");
+	}
+
+	@Override
+	public void reprova(Orcamento orcamento) {
+		throw new RuntimeException("Orçamento está finalizado");
+	}
+
+	@Override
+	public void finaliza(Orcamento orcamento) {
+		throw new RuntimeException("Orçamento está finalizado");
+	}
+
+}
