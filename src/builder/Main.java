56d9c9cdde377a489945bd64095a264ddcc94fd3
gustavo <gustavodreche@msn.com>
2020-06-15 16:45:33 -0300

    Padrao Builder

---
+++ src/builder/Main.java
@@ -0,0 +1,22 @@
+package builder;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		@SuppressWarnings("unused")
+		NotaFiscal notaFiscal = new NotaFiscalBuilder()
+				.setRazaoSocial("123456789")
+				.setCnpj("123456789")
+				.setDataDeEmissao(LocalDateTime.now())
+				.setImpostos(10)
+				.setItens(new LinkedList<ItemDaNota>())
+				.setObservacoes("Autorizado")
+				.setValorBruto(50)
+				.retornaObjeto();
+	}
+
+}
---
+++ src/builder/NotaFiscal.java
@@ -0,0 +1,45 @@
+package builder;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SuppressWarnings("unused")
+public class NotaFiscal {
+
+	private String razaoSocial;
+	private String cnpj;
+	private LocalDateTime dataDeEmissao;
+	private double valorBruto;
+	private double impostos;
+	private List<ItemDaNota> itens;
+	private String observacoes;
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public void setDataDeEmissao(LocalDateTime dataDeEmissao) {
+		this.dataDeEmissao = dataDeEmissao;
+	}
+
+	public void setValorBruto(double valorBruto) {
+		this.valorBruto = valorBruto;
+	}
+
+	public void setImpostos(double impostos) {
+		this.impostos = impostos;
+	}
+
+	public void setItens(List<ItemDaNota> itens) {
+		this.itens = itens;
+	}
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+
+}
